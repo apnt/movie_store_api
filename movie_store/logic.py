42148667a8ca37ac224d981ed503ba5596d27f3f
Apostolis Ntarampekis <apostolis.ntarampekis@fieldscale.com>
2021-04-12 23:46:22 +0300

    Handle negative rental charge

--- movie_store/logic.py
+++ movie_store/logic.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 
 def calculate_charge(rental, initial_charge=1.0, default_charge=0.5, initial_charge_days=3):
     current_time = timezone.now()
+    if current_time < rental.rental_date:
+        return 0.0
     time_diff = current_time - rental.rental_date
     number_of_days = ceil(time_diff.total_seconds() / 86400)
     if number_of_days > initial_charge_days:
